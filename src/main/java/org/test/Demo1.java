01a72369af34969e1ecf42531fb9a13e838b755a
sunshiqi <18338830119@163com>
2021-05-25 10:28:43 +0800

    this is second commit

--- src/main/java/org/test/Demo1.java
+++ src/main/java/org/test/Demo1.java
@@ -3,5 +3,6 @@ package org.test;
 public class Demo1 {
     public static void main(String[] args) {
         System.out.println("this is git test");
+        System.out.println("this is second push");
     }
 }
